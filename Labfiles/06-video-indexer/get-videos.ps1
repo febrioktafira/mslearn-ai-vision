691c050754eb436088b7507ea770400a36c4eee4
febrioktafira <febry.of31@gmail.com>
2025-04-20 17:57:40 +0700

    In Labfiles 06-video-indexer, add video player and insights iframes to analyze-video.html, update account credentials in get-videos.ps1, and create mslearn-ai-vision solution file

--- Labfiles/06-video-indexer/get-videos.ps1
+++ Labfiles/06-video-indexer/get-videos.ps1
@@ -1,5 +1,5 @@
-$account_id="YOUR_ACCOUNT_ID"
-$api_key="YOUR_API_KEY"
+$account_id="451b2c05-59f8-4883-85ea-655920559de7"
+$api_key="1b8ebba4d2a44813974796f4688a5053"
 $location="trial"
 
 # Call the AccessToken method with the API key in the header to get an access token
